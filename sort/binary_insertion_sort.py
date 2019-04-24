b785edb4d872fad9fdb0cd8b71dc6a2d52526998
shuvava <shuvava@users.noreply.github.com>
2019-04-24 06:24:36 -0700

    small refactoring

--- sort/binary_insertion_sort.py
+++ sort/binary_insertion_sort.py
@@ -23,25 +23,23 @@ def binary_search(alist, key, start, end):
     if end - start <= 1:
         if key < alist[start]:
             return start - 1
-        else:
-            return start
- 
+        return start
+
     mid = (start + end)//2
     if alist[mid] < key:
         return binary_search(alist, key, mid, end)
-    elif alist[mid] > key:
+    if alist[mid] > key:
         return binary_search(alist, key, start, mid)
-    else:
-        return mid
+    return mid
 
 def binary_insertion_sort(alist):
     for i in range(1, len(alist)):
         temp = alist[i]
         pos = binary_search(alist, temp, 0, i) + 1
- 
+
         for k in range(i, pos, -1):
             alist[k] = alist[k - 1]
- 
+
         alist[pos] = temp
  
 if __name__ == '__main__':
