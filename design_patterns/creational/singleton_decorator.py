51a84f90ef384aecc098e47031053bc89affcef7
shuvava <shuvava@users.noreply.github.com>
2019-08-10 06:17:10 -0700

    add singleton pattern example

---
+++ design_patterns/creational/singleton_decorator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2019 Vladimir Shurygin.  All rights reserved.
+#
+
+def singleton(cls):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
+
+@singleton
+class MyClass:
+    pass
+
+if __name__ == "__main__":
+    s1 = MyClass()
+    s2 = MyClass()
+    print(f's1={id(s1)}; s2={id(s2)}')
