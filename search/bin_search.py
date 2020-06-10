9edd14e39288bd925498d0cf159582575cfad2a3
shuvava <shuvava@users.noreply.github.com>
2020-06-10 05:07:56 -0700

    fix bin search

--- search/bin_search.py
+++ search/bin_search.py
@@ -25,11 +25,12 @@ def search(nums: List[int], target: int) -> int:
             high = mid - 1
         else:
             low = mid + 1
-    return -1
+    return low
 
 
 if __name__ == '__main__':
     test_cases = [
+        ([1, 3, 5, 6], 2, 1),
         ([1, 3, 5, 9], 9, 3),
         ([1, 3, 5, 9], 5, 2),
     ]
