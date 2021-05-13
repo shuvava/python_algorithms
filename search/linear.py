735eec4299209ce0e8fdcef3b433f8e4fff3a695
shuvava <shuvava@users.noreply.github.com>
2021-05-13 05:42:46 -0700

    fix small style issues add two problems

--- search/linear.py
+++ search/linear.py
@@ -1,19 +1,20 @@
-''' Implementation of sentinel linear search
+""" Implementation of sentinel linear search
 page 16 of Algorithms Unlocked(2013)
 Author Vlad Shurygin 09/18/2017
-'''
+"""
 
 from base_interface import BaseAlg
 
+
 class Linear(BaseAlg):
-    ''' Implematation of sentinel linear search'''
+    """ Implementation of sentinel linear search"""
     def set_console_args(self, parser):
-        ''' adds console parameters  into ArgumentParser object'''
+        """ adds console parameters  into ArgumentParser object"""
         return parser
 
     @staticmethod
     def search(array, x):
-        '''sentinel-linear-search implementation
+        """sentinel-linear-search implementation
         Parameters
         ----------
         array: list
@@ -22,7 +23,7 @@ class Linear(BaseAlg):
         Returns
         -------
             index of element 'x' in lint 'array'
-        '''
+        """
         last = array[-1]
         array[-1] = x
         i = 0
@@ -35,11 +36,12 @@ class Linear(BaseAlg):
         return None
 
     def main(self):
-        '''main entry point'''
+        """main entry point"""
         _x, _t = self.get_array()
         result = self.search(_t, _x)
         print('------------------------------------------')
         print(result, _t[result])
+
 
 runner = Linear()
 runner.run()
--- sort/counting.py
+++ sort/counting.py
@@ -17,13 +17,13 @@ from base_interface import BaseAlg  # pylint: disable=C0413
 
 
 class CountingSort(BaseAlg):
-    ''' Implementation of Counting Sort
+    """ Implementation of Counting Sort
     Thomas H. Cormen Algorithms Unlocked (2013) page 67
-    '''
+    """
 
     @staticmethod
     def count_keys_equal_or_less(_array, length, val_range):
-        '''computing how many elements have sort
+        """computing how many elements have sort
         keys equal or less to that value
 
         :parameters:
@@ -34,7 +34,7 @@ class CountingSort(BaseAlg):
         :returns: *list* - An array equal_or_less[0..valRange-1] such that equal_or_less[j]
         contains the number of elements of A that equal or less j ,
         for j = 0,1,2...,valRange-1.
-        '''
+        """
         _keys = [0] * val_range
         # COUNT-KEYS-EQUAL
         for inx in range(0, length):
@@ -51,7 +51,7 @@ class CountingSort(BaseAlg):
 
     @staticmethod
     def rearrange(_array, _keys, length):
-        '''rearrange elements in original array
+        """rearrange elements in original array
 
         :parameters:
         _array: *list* - array of elements
@@ -59,7 +59,7 @@ class CountingSort(BaseAlg):
         length: *Number* - length of sorting array
 
         :returns: *list* - An array of sorted elements
-        '''
+        """
         result = [0] * length
         for inx in range(0, length):
             key = _array[inx]
@@ -69,7 +69,7 @@ class CountingSort(BaseAlg):
         return result
 
     def main(self, _array):
-        '''main entry point'''
+        """main entry point"""
         keys = self.count_keys_equal_or_less(_array, len(_array), max(_array))
         result = self.rearrange(_array, keys, len(_array))
         if self.verbosity:
